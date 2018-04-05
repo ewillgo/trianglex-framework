9db77da02fabef85f4d57930653869a63e70d672
ewill.leung@foxmail.com <will89liangwenwei>
2018-04-05 18:55:25 +0800

    ...

---
+++ common/src/main/java/org/trianglex/common/security/WebSecurityAdapter.java
@@ -0,0 +1,19 @@
+package org.trianglex.common.security;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class WebSecurityAdapter extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf()
+                .disable()
+                .authorizeRequests()
+                .antMatchers("/actuator/**")
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+
+}
