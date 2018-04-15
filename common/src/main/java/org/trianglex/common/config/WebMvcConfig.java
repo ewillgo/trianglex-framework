7eec4dcbba423b352dd814e31698838acb34ea8b
ewill.leung@foxmail.com <will89liangwenwei>
2018-04-15 11:51:56 +0800

    ...

--- common/src/main/java/org/trianglex/common/config/WebMvcConfig.java
+++ common/src/main/java/org/trianglex/common/config/WebMvcConfig.java
@@ -9,6 +9,7 @@ import org.springframework.http.converter.ByteArrayHttpMessageConverter;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.http.converter.xml.MappingJackson2XmlHttpMessageConverter;
 import org.springframework.web.accept.ContentNegotiationManager;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -32,10 +33,13 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Autowired
     private ByteArrayHttpMessageConverter byteArrayHttpMessageConverter;
 
+    @Autowired
+    private MappingJackson2XmlHttpMessageConverter mappingJackson2XmlHttpMessageConverter;
+
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
         configurer.defaultContentTypeStrategy(new ContentNegotiationManager())
-                .ignoreAcceptHeader(true)
+                .ignoreAcceptHeader(false)
                 .useRegisteredExtensionsOnly(false);
     }
 
@@ -45,6 +49,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
         converters.add(mappingJackson2HttpMessageConverter);
         converters.add(stringHttpMessageConverter);
         converters.add(byteArrayHttpMessageConverter);
+        converters.add(mappingJackson2XmlHttpMessageConverter);
         logger.info("SpringBoot message converters: {}",
                 converters.stream().map((converter) -> converter.getClass().getSimpleName()).collect(Collectors.joining(", ")));
     }
