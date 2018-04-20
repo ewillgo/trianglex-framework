96ed0b8529ce061f58a97c27cce03a952a596f55
liangwenwei <liangwenwei@tuandai.com>
2018-04-20 08:56:14 +0800

    ...

--- common/src/main/java/org/trianglex/common/dto/DtoAttributes.java
+++ common/src/main/java/org/trianglex/common/dto/DtoAttributes.java
@@ -8,8 +8,6 @@ public interface DtoAttributes<T> extends Serializable {
 
     /**
      * DTO object to PO object
-     *
-     * @return
      */
     default T toPO(T t) {
         BeanUtils.copyProperties(this, t);
