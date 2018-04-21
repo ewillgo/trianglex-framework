4bad59325d3eed0e463dde08d9603fee2de2bcf7
ewill.leung@foxmail.com <will89liangwenwei>
2018-04-21 17:20:21 +0800

    ...

--- common/src/main/java/org/trianglex/common/util/PasswordUtils.java
+++ common/src/main/java/org/trianglex/common/util/PasswordUtils.java
@@ -1,10 +1,11 @@
 package org.trianglex.common.util;
 
+import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.codec.digest.Crypt;
-import org.springframework.util.Base64Utils;
 
 import java.security.SecureRandom;
 import java.util.Random;
+import java.util.regex.Pattern;
 
 public abstract class PasswordUtils {
 
@@ -12,6 +13,7 @@ public abstract class PasswordUtils {
     private static final String SHA256_CRYPT_PREFIX = "$5$";
     private static final String SHA512_CRYPT_PREFIX = "$6$";
     private static final Random SECURE_RANDOM = new SecureRandom();
+    private static final Pattern SALT_PATTERN = Pattern.compile("^\\$([56])\\$(rounds=(\\d+)\\$)?([\\.\\/a-zA-Z0-9]{1,16}).*");
 
     private PasswordUtils() {
 
@@ -22,14 +24,21 @@ public abstract class PasswordUtils {
     }
 
     public static String salt256() {
-        byte[] salt = new byte[SALT_BYTES];
-        SECURE_RANDOM.nextBytes(salt);
-        return SHA256_CRYPT_PREFIX + Base64Utils.encodeToString(salt);
+        return salt(SHA256_CRYPT_PREFIX);
     }
 
     public static String salt512() {
-        byte[] salt = new byte[SALT_BYTES];
-        SECURE_RANDOM.nextBytes(salt);
-        return SHA512_CRYPT_PREFIX + Base64Utils.encodeToString(salt);
+        return salt(SHA512_CRYPT_PREFIX);
     }
+
+    private static String salt(String cryptPrefix) {
+        String saltString;
+        byte[] salt = new byte[SALT_BYTES];
+        do {
+            SECURE_RANDOM.nextBytes(salt);
+            saltString = cryptPrefix + Base64.encodeBase64String(salt);
+        } while (!SALT_PATTERN.matcher(saltString).find());
+        return saltString;
+    }
+
 }
