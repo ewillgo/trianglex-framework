8b938167c1c3dbf00f195b0773856b94e925ce02
ewill.leung@foxmail.com <will89liangwenwei>
2018-04-21 17:43:12 +0800

    ...

--- common/src/main/java/org/trianglex/common/util/PasswordUtils.java
+++ common/src/main/java/org/trianglex/common/util/PasswordUtils.java
@@ -4,7 +4,6 @@ import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.codec.digest.Crypt;
 
 import java.security.SecureRandom;
-import java.util.Random;
 import java.util.regex.Pattern;
 
 public abstract class PasswordUtils {
@@ -12,7 +11,6 @@ public abstract class PasswordUtils {
     private static final int SALT_BYTES = 16;
     private static final String SHA256_CRYPT_PREFIX = "$5$";
     private static final String SHA512_CRYPT_PREFIX = "$6$";
-    private static final Random SECURE_RANDOM = new SecureRandom();
     private static final Pattern SALT_PATTERN = Pattern.compile("^\\$([56])\\$(rounds=(\\d+)\\$)?([\\.\\/a-zA-Z0-9]{1,16}).*");
 
     private PasswordUtils() {
@@ -34,8 +32,10 @@ public abstract class PasswordUtils {
     private static String salt(String cryptPrefix) {
         String saltString;
         byte[] salt = new byte[SALT_BYTES];
+        SecureRandom secureRandom = new SecureRandom(
+                String.valueOf(System.currentTimeMillis() + Runtime.getRuntime().freeMemory()).getBytes());
         do {
-            SECURE_RANDOM.nextBytes(salt);
+            secureRandom.nextBytes(salt);
             saltString = cryptPrefix + Base64.encodeBase64String(salt);
         } while (!SALT_PATTERN.matcher(saltString).find());
         return saltString;
