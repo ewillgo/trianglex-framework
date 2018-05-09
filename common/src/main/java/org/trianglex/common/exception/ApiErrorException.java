c30f0941d052cdb93b53b6aafd2c90a433b5751a
ewill.leung@foxmail.com <will89liangwenwei>
2018-05-09 22:58:52 +0800

    ...

---
+++ common/src/main/java/org/trianglex/common/exception/ApiErrorException.java
@@ -0,0 +1,53 @@
+package org.trianglex.common.exception;
+
+public class ApiErrorException extends RuntimeException {
+
+    private Throwable original;
+    private ApiCode apiCode;
+    private Object data;
+
+    public ApiErrorException(String message) {
+        super(message);
+    }
+
+    public ApiErrorException(String message, Throwable cause) {
+        super(message, cause);
+        this.original = cause;
+    }
+
+    public ApiErrorException(ApiCode apiCode) {
+        super(apiCode.getMessage());
+        this.apiCode = apiCode;
+    }
+
+    public ApiErrorException(ApiCode apiCode, Throwable cause) {
+        super(apiCode.getMessage(), cause);
+        this.apiCode = apiCode;
+        this.original = cause;
+    }
+
+    public ApiErrorException(ApiCode apiCode, Object data) {
+        super(apiCode.getMessage());
+        this.apiCode = apiCode;
+        this.data = data;
+    }
+
+    public ApiErrorException(ApiCode apiCode, Object data, Throwable cause) {
+        super(apiCode.getMessage(), cause);
+        this.apiCode = apiCode;
+        this.data = data;
+        this.original = cause;
+    }
+
+    public Throwable getOriginal() {
+        return original;
+    }
+
+    public ApiCode getApiCode() {
+        return apiCode;
+    }
+
+    public Object getData() {
+        return data;
+    }
+}
