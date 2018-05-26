ec623aa36c8743ab122b3bd4ee17e93b9a7ef296
liangwenwei <liangwenwei@tuandai.com>
2018-05-26 17:26:47 +0800

    ...

---
+++ common/src/main/java/org/trianglex/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,6 @@
+package org.trianglex.common.exception;
+
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+}
